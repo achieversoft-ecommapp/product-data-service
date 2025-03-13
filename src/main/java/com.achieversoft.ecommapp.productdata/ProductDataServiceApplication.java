f074c29852f3b0cb3e1d14c796100ef55791db20
prsgithub <ranjansahoo.prakash@gmail.com>
2025-03-14 00:10:20 +0530

    1st commit

---
+++ src/main/java/com.achieversoft.ecommapp.productdata/ProductDataServiceApplication.java
@@ -0,0 +1,13 @@
+package com.achieversoft.ecommapp.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductDataServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductDataServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/achieversoft/ecommapp/productdata/ProductDataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.achieversoft.ecommapp.productdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductDataServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
